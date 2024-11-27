3a9388c6379b1dc95865b026f8eb723995d32274
Dietrich Daroch <dietr1ch@acm.org>
2024-11-27 14:59:49 -0300

    Keyboard,Dvorak,Fix: Fix virtual console keymap

--- system/input/keyboard/dvorak.nix
+++ system/input/keyboard/dvorak.nix
@@ -1,16 +1,38 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   console = {
-    keyMap = "dvorak-programmer";
+    # This is supposed to be listed in,
+    #   ls $(nix-build --no-out-link '<nixpkgs>' -A xkeyboard_config)/etc/X11/xkb/rules/base.lst
+    #
+    # - dvorak-intl     us: English (Dvorak, intl., with dead keys)
+    # - dvorak-alt-intl us: English (Dvorak, alt. intl.)
+    # - dvp             us: English (programmer Dvorak)
+    # - dvorak          de: German (Dvorak)
+    # - dvorak          es: Spanish (Dvorak)
+    # - dvorak          latam: Spanish (Latin American, Dvorak)
+    #
+    # However, that doesn't work
+    #
+    # https://search.nixos.org/options?channel=unstable&query=console.keyMap
+    keyMap = lib.mkDefault "dvorak-programmer";
+    packages = with pkgs; [
+      kbd
+    ];
   };
+
   services = {
     xserver = {
       exportConfiguration = true;
       xkb = {
         layout = "us";
         variant = "dvp";
-        options = "compose:ralt, caps:ctrl_modifier, grp_led:caps, grp:shifts_toggle";
+        options = lib.strings.concatStringsSep ", " [
+          "compose:ralt"
+          "caps:ctrl_modifier"
+          "grp_led:caps"
+          "grp:shifts_toggle"
+        ];
       };
     };
   };
