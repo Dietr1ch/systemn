5983dbbfd5d7f87727329b35406527d553293f04
Dietrich Daroch <dietr1ch@acm.org>
2024-11-27 13:12:16 -0300

    Desktop,Sound: Add wireplumber

--- desktop/sound.nix
+++ desktop/sound.nix
@@ -4,6 +4,7 @@
   security.rtkit.enable = true;
 
   services = {
+    # https://search.nixos.org/options?channel=unstable&query=services.pipewire
     pipewire = {
       enable = true;
 
@@ -13,6 +14,13 @@
       };
       pulse.enable = true;
       jack.enable = true;
+
+      wireplumber = {
+        enable = true;
+        extraLv2Packages = with pkgs; [
+          lsp-plugins  # https://search.nixos.org/packages?channel=unstable&query=lsp-plugins
+        ];
+      };
     };
   };
 }
