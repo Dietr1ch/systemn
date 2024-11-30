3475f4d8a617e24cadbf807404a0f7d50aa78925
Dietrich Daroch <dietr1ch@acm.org>
2024-11-29 22:18:48 -0300

    Memory: Set up systemd-oomd to auto instead of kill.
    It's really trigger happy otherwise.

--- desktop/shell/graphical/login-sddm.nix
+++ desktop/shell/graphical/login-sddm.nix
@@ -9,5 +9,16 @@
       };
     };
   };
+
+  systemd = {
+    services = {
+      display-manager = {
+        # https://www.freedesktop.org/software/systemd/man/systemd.service.html
+        serviceConfig = {
+          "OOMScoreAdjust" = -800;
+        };
+      };
+    };
+  };
 }
 
--- system/memory.nix
+++ system/memory.nix
@@ -19,18 +19,54 @@
     ];
   };
 
-  # systemd = {
-  #   # https://search.nixos.org/options?channel=unstable&query=systemd.oomd
-  #   oomd = {
-  #     enableRootSlice = true;
-  #     enableSystemSlice = true;
-  #     enableUserSlices = true;
-  #
-  #     # https://man.archlinux.org/man/systemd-oomd.8
-  #     extraConfig = ''
-  #     '';
-  #   };
-  # };
+  systemd = {
+    # https://search.nixos.org/options?channel=unstable&query=systemd.oomd
+    oomd = {
+      enableRootSlice = true;
+      enableSystemSlice = true;
+      enableUserSlices = true;
+
+      # https://man.archlinux.org/man/systemd-oomd.8
+      # https://man.archlinux.org/man/oomd.conf.5
+
+      extraConfig = {
+        "DefaultMemoryPressureDurationSec" = "20s";
+      };
+    };
+
+    slices = {
+      "-" = {
+        sliceConfig = {
+          "ManagedOOMMemoryPressure" = lib.mkForce "auto";
+          "ManagedOOMMemoryPressureLimit" = "95%";
+          "ManagedOOMPreference" = "none";
+          "ManagedOOMSwap" = "auto";
+          "MemoryPressureThresholdUSec" = "200ms";
+          "MemoryPressureWatch" = "auto";
+        };
+      };
+      "system" = {
+        sliceConfig = {
+          "ManagedOOMMemoryPressure" = lib.mkForce "auto";
+          "ManagedOOMMemoryPressureLimit" = "95%";
+          "ManagedOOMPreference" = "none";
+          "ManagedOOMSwap" = "auto";
+          "MemoryPressureThresholdUSec" = "200ms";
+          "MemoryPressureWatch" = "auto";
+        };
+      };
+      "user-" = {
+        sliceConfig = {
+          "ManagedOOMMemoryPressure" = lib.mkForce "auto";
+          "ManagedOOMMemoryPressureLimit" = "95%";
+          "ManagedOOMPreference" = "none";
+          "ManagedOOMSwap" = "auto";
+          "MemoryPressureThresholdUSec" = "200ms";
+          "MemoryPressureWatch" = "auto";
+        };
+      };
+    };
+  };
 
   environment = {
     systemPackages = with pkgs; [
