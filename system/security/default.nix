4aaf109695b9fc5f91b21326d180b8ef5c67f95e
Dietrich Daroch <Dietrich@Daroch.me>
2024-12-07 21:07:39 -0800

    Security: Use sudo-rs

--- system/security/default.nix
+++ system/security/default.nix
@@ -4,5 +4,18 @@
   imports = [
     ./cgroups.nix
     ./limits.nix
+    # ./yubikey.nix
   ];
+
+  # https://search.nixos.org/options?channel=unstable&query=security
+  security = {
+    # https://search.nixos.org/options?channel=unstable&query=security.auditd
+    auditd = {
+      enable = true;
+    };
+    # https://search.nixos.org/options?channel=unstable&query=security.sudo-rs
+    sudo-rs = {
+      enable = true;
+    };
+  };
 }
