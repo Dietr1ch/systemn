3c8732d28b5b7a636f819256a1a48a48099ec6d4
Dietrich Daroch <Dietrich@Daroch.me>
2025-03-16 21:07:27 -0300

    Server,Transmission: Open peering ports

--- server/transmission.nix
+++ server/transmission.nix
@@ -3,14 +3,18 @@
 {
   services = {
     # https://search.nixos.org/options?channel=unstable&query=services.transmission
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/torrent/transmission.nix
     transmission = {
       enable = true;
 
       package = pkgs.transmission_4;
 
+      openPeerPorts = true;
       settings = {
         # https://github.com/transmission/transmission/wiki/Editing-Configuration-Files
         peer-port-random-on-start = true;
+        incomplete-dir-enabled = true;
+        watch-dir-enabled = true;
       };
     };
   };
