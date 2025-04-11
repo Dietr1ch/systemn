7467559fa8edfd06b7266631c50e081d21caa529
Dietrich Daroch <Dietrich@Daroch.me>
2025-04-11 00:22:15 -0400

    Info: Add LTTng modules

--- system/base/information.nix
+++ system/base/information.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
+  boot = {
+    extraModulePackages = [
+      config.boot.kernelPackages.lttng-modules
+    ];
+  };
+
   environment = {
     systemPackages = with pkgs; [
       htop
