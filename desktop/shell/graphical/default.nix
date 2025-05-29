b3b3cf6959153851767a6af360684d953ced77c3
Dietrich Daroch <Dietrich@Daroch.me>
2025-05-29 18:51:46 -0400

    Desktop: Add wayland and pipewire hints

--- desktop/shell/graphical/default.nix
+++ desktop/shell/graphical/default.nix
@@ -15,5 +15,19 @@
       enable = true;
     };
   };
+
+  environment = {
+    variables = {
+      # SDL2 (https://github.com/libsdl-org/SDL/blob/SDL2/include/SDL_hints.h)
+      "SDL_VIDEODRIVER" = "wayland";
+      "SDL_AUDIODRIVER" = "pipewire";
+      # SDL3 (https://github.com/libsdl-org/SDL/blob/main/include/SDL3/SDL_hints.h)
+      "SDL_VIDEO_DRIVER" = "wayland";
+      "SDL_AUDIO_DRIVER" = "pipewire";
+
+      "QT_QPA_PLATFORM" = "wayland";
+      "ELECTRON_OZONE_PLATFORM_HINT" = "wayland";
+    };
+  };
 }
 
