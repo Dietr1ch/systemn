2f50d2804ce9c11ed5a9e6a3907083a615634b4e
Dietrich Daroch <Dietrich@Daroch.me>
2025-06-10 17:51:06 -0400

    System,Memory: Fix slice names and add links

--- system/memory.nix
+++ system/memory.nix
@@ -1,12 +1,17 @@
-{ config, pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   systemd = {
     # https://search.nixos.org/options?channel=unstable&query=systemd.oomd
     oomd = {
-      enableRootSlice = true;
-      enableSystemSlice = true;
-      enableUserSlices = true;
+      # Run on all slices
+      enableRootSlice = true; # /etc/systemd/system/-.slice
+      enableSystemSlice = true; # /etc/systemd/system/system.slice
+      enableUserSlices = true; # /etc/systemd/system/user.slice
 
       # https://man.archlinux.org/man/systemd-oomd.8
       # https://man.archlinux.org/man/oomd.conf.5
@@ -16,7 +21,9 @@
       };
     };
 
+    # https://search.nixos.org/options?channel=unstable&query=systemd.slices
     slices = {
+      # /etc/systemd/system/-.slice
       "-" = {
         sliceConfig = {
           "ManagedOOMMemoryPressure" = lib.mkForce "auto";
@@ -26,6 +33,8 @@
           "MemoryPressureWatch" = "auto";
         };
       };
+
+      # /etc/systemd/system/system.slice
       "system" = {
         sliceConfig = {
           "ManagedOOMMemoryPressure" = lib.mkForce "auto";
@@ -35,8 +44,12 @@
           "MemoryPressureWatch" = "auto";
         };
       };
-      "user-" = {
+
+      # /etc/systemd/system/user.slice
+      # /etc/systemd/system/user@.service
+      "user" = {
         sliceConfig = {
+          # https://man.archlinux.org/man/systemd.resource-control.5.en#Memory_Pressure_Control
           "ManagedOOMMemoryPressure" = lib.mkForce "auto";
           "ManagedOOMMemoryPressureLimit" = 0;
           "ManagedOOMPreference" = "none";
