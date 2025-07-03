8c5c32e11a51c0ca3db39e33627a6bd6c2da10d5
Dietrich Daroch <Dietrich@Daroch.me>
2025-07-02 22:12:57 -0400

    System,OOMd: Enable and adjust OOMd

--- system/memory.nix
+++ system/memory.nix
@@ -13,47 +13,52 @@
       enableSystemSlice = true; # /etc/systemd/system/system.slice
       enableUserSlices = true; # /etc/systemd/system/user.slice
 
+      # /etc/systemd/oomd.conf
       # https://man.archlinux.org/man/systemd-oomd.8
-      # https://man.archlinux.org/man/oomd.conf.5
-
+      # https://man.archlinux.org/man/oomd.conf.5.en
       extraConfig = {
         "DefaultMemoryPressureDurationSec" = "20s";
+        "DefaultMemoryPressureLimit" = "60%";
+        "SwapUsedLimit" = "80%";
       };
     };
 
     # https://search.nixos.org/options?channel=unstable&query=systemd.slices
+    # https://www.freedesktop.org/software/systemd/man/latest/systemd.resource-control.html
     slices = {
+      # Root slice
       # /etc/systemd/system/-.slice
       "-" = {
         sliceConfig = {
           "ManagedOOMMemoryPressure" = lib.mkForce "auto";
           "ManagedOOMMemoryPressureLimit" = 0;
           "ManagedOOMPreference" = "none";
-          "ManagedOOMSwap" = "auto";
+          "ManagedOOMSwap" = "kill";
           "MemoryPressureWatch" = "auto";
         };
       };
 
+      # System slice
       # /etc/systemd/system/system.slice
       "system" = {
         sliceConfig = {
-          "ManagedOOMMemoryPressure" = lib.mkForce "auto";
-          "ManagedOOMMemoryPressureLimit" = 0;
+          "ManagedOOMMemoryPressure" = lib.mkForce "kill";
+          "ManagedOOMMemoryPressureLimit" = "60%";
           "ManagedOOMPreference" = "none";
-          "ManagedOOMSwap" = "auto";
+          "ManagedOOMSwap" = "kill";
           "MemoryPressureWatch" = "auto";
         };
       };
 
+      # User slices
       # /etc/systemd/system/user.slice
       # /etc/systemd/system/user@.service
       "user" = {
         sliceConfig = {
-          # https://man.archlinux.org/man/systemd.resource-control.5.en#Memory_Pressure_Control
-          "ManagedOOMMemoryPressure" = lib.mkForce "auto";
-          "ManagedOOMMemoryPressureLimit" = 0;
+          "ManagedOOMMemoryPressure" = lib.mkForce "kill";
+          "ManagedOOMMemoryPressureLimit" = "80%";
           "ManagedOOMPreference" = "none";
-          "ManagedOOMSwap" = "auto";
+          "ManagedOOMSwap" = "kill";
           "MemoryPressureThresholdUSec" = "200ms";
           "MemoryPressureWatch" = "auto";
         };
