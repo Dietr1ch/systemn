5743be448438f6349557f052473f59a96f7fb76d
Dietrich Daroch <Dietrich@Daroch.me>
2025-08-11 23:52:19 -0400

    Misc: Update renamed settings.
    - `systemd.watchdog.kexecTime' defined in `system/base/watchdog.nix' has been renamed to `systemd.settings.Manager.KExecWatchdogSec'.
    - `systemd.watchdog.rebootTime' defined in `system/base/watchdog.nix' has been renamed to `systemd.settings.Manager.RebootWatchdogSec'.
    - `systemd.watchdog.runtimeTime' defined in `source/system/base/watchdog.nix' has been renamed to `systemd.settings.Manager.RuntimeWatchdogSec'.

--- system/base/watchdog.nix
+++ system/base/watchdog.nix
@@ -1,24 +1,27 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   systemd = {
     # https://search.nixos.org/options?channel=unstable&query=systemd.watchdog
     # https://www.freedesktop.org/software/systemd/man/latest/systemd-system.conf.html#Hardware%20Watchdog
-    watchdog = {
-      # The amount of time which can elapse before a watchdog hardware device
-      # will automatically reboot the system.
-      runtimeTime = "20s";
+    settings = {
+      Manager = {
+        # The amount of time which can elapse before a watchdog hardware device
+        # will automatically reboot the system.
+        RuntimeWatchdogSec = "20s";
 
-      # The amount of time which can elapse after a reboot has been triggered
-      # before a watchdog hardware device will automatically reboot the system.
-      # If left null, systemd will use its default of 10 minutes; see
-      # systemd-system.conf(5).
-      rebootTime = "5m";
-      # The amount of time which can elapse when kexec is being executed before
-      # a watchdog hardware device will automatically reboot the system. This
-      # option should only be enabled if reloadTime is also enabled; see
-      # kexec(8).
-      kexecTime = "5m";
+        # The amount of time which can elapse after a reboot has been triggered
+        # before a watchdog hardware device will automatically reboot the
+        # system. If left null, systemd will use its default of 10 minutes; see
+        # systemd-system.conf(5).
+        RebootWatchdogSec = "5m";
+
+        # The amount of time which can elapse when kexec is being executed
+        # before a watchdog hardware device will automatically reboot the
+        # system. This option should only be enabled if reloadTime is also
+        # enabled; see kexec(8).
+        KExecWatchdogSec = "5m";
+      };
     };
   };
 }
