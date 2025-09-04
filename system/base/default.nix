e283eb71fa10f0c5a10c33174b3a732d4e1fa45e
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-04 14:35:37 -0400

    Set up strict action timeouts.
    I don't want to wait 90s to get a failure. Units halting their dependencies
    for long cause more trouble hogging the system than themselves not being up.

--- system/base/default.nix
+++ system/base/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
@@ -18,4 +18,22 @@
 
   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/system/dbus.nix
   services.dbus.implementation = "broker";
+
+  # systemd.user.settings
+  systemd = {
+    # /etc/systemd/user.conf
+
+    user = {
+      # https://search.nixos.org/options?channel=unstable&show=systemd.user.extraConfig
+      extraConfig = ''
+        # https://www.mankier.com/5/systemd-system.conf
+
+        # https://www.mankier.com/5/systemd-system.conf#Timeouts_and_Rate_Limits
+        DefaultTimeoutStartSec=5s
+        DefaultTimeoutStopSec=20s
+        DefaultTimeoutAbortSec=20s
+        # DefaultRestartSec=100ms
+      '';
+    };
+  };
 }
