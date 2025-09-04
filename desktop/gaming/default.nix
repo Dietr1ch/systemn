840f0148d45442d17a47292fe7b49de9fedffa64
Dietrich Daroch <Dietrich@Daroch.me>
2025-09-03 22:15:36 -0400

    Remove redundant config that was moved to OBS

--- desktop/gaming/default.nix
+++ desktop/gaming/default.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -14,19 +10,7 @@
   boot = {
     kernelModules = [
       "hid-nintendo"
-      "v4l2loopback"
     ];
-    extraModulePackages = [
-      config.boot.kernelPackages.v4l2loopback
-    ];
-    # v4l2loopback. https://gist.github.com/TheSirC/93130f70cc280cdcdff89faf8d4e98ab
-    # https://github.com/umlaeute/v4l2loopback
-    #
-    # Creates a single loopback on /sys/devices/virtual/video4linux/video9/
-    #   exclusive_caps=1 might be needed for Chrome/WebRTC detection
-    extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 video_nr=9 card_label=loopback_cam
-    '';
   };
 
   users = {
