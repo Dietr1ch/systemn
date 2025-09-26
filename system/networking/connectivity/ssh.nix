1d6627248d989b6097fac0e9b346f4b6b9881492
Dietrich Daroch <dietrich@daroch.me>
2025-09-26 20:04:09 +0000

    Add gnupg agent
    Using gnupg agent on userland leads to annoying breakages as the user and system are updated separately

--- system/networking/connectivity/ssh.nix
+++ system/networking/connectivity/ssh.nix
@@ -10,7 +10,27 @@
       enable = true;
     };
     # Key agents on ../../security/yubikey.nix
-  };
+
+    # https://search.nixos.org/options?channel=unstable&show=programs.gnupg.agent.enable&query=programs.gnupg
+    gnupg = {
+      agent = {
+        enable = true;
+
+        # pinentryPackage = "??"  # User controlled
+
+        enableSSHSupport = true;
+        enableExtraSocket = true;
+        enableBrowserSocket = true;
+
+        # https://www.gnupg.org/documentation/manuals/gnupg26/gpg-agent.1.html
+        settings = {
+          default-cache-ttl = 300; # 5m
+        };
+      };
+    };
+   }; # ..programs.gnupg
+
+  }; # ..programs
 
   services = {
     openssh = {
@@ -48,3 +68,5 @@
     ];
   };
 }
+
+
