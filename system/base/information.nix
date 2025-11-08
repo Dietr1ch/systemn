b8af9cf8a31e459f430017ae76bec73f56b99657
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-08 15:24:15 -0300

    Info: Try out rust-motd

--- system/base/information.nix
+++ system/base/information.nix
@@ -1,6 +1,18 @@
 { pkgs, ... }:
 
 {
+  programs = {
+    # https://search.nixos.org/options?channel=unstable&show=programs.rust-motd
+    rust-motd = {
+      enable = true;
+
+      enableMotdInSSHD = true;
+      # https://github.com/rust-motd/rust-motd/blob/main/README.md#configuration
+      # settings = {
+      # };
+    };
+  };
+
   environment = {
     systemPackages = with pkgs; [
       htop
