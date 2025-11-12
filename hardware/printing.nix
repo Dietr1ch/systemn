76750008ce7d5f56ee2f0abc0f9572e43554f81a
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-12 16:45:47 -0300

    Misc: Add links

--- hardware/printing.nix
+++ hardware/printing.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 
+# https://nixos.wiki/wiki/Printing
 {
   services = {
     printing = {
       enable = true;
 
+      # https://nixos.wiki/wiki/Printing#Drivers
       drivers = with pkgs; [
         gutenprintBin
       ];
