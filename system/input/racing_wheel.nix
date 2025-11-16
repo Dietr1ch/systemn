e05743b9bb96aa7ffb2e491527e4a29672968f96
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-15 21:51:31 -0300

    Input,RacingWheel: Drop kernel module that's incompatible with custom kernels

--- system/input/racing_wheel.nix
+++ system/input/racing_wheel.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 
 {
+  # NOTE: Kernel modules are kernel specific :/
   boot = {
     kernelModules = [
       # "new-lg4ff"
     ];
-    extraModulePackages = with pkgs; [
-      linuxPackages.new-lg4ff
-    ];
+    # extraModulePackages = with pkgs; [
+    #   # linuxPackages.new-lg4ff
+    # ];
   };
 
   environment = {
