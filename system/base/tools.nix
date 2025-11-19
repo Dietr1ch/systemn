68986a459549f00c4b2848f5a8bcb25a4660c9e4
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-18 21:16:00 -0300

    Base,Tools: Configure less flags

--- system/base/tools.nix
+++ system/base/tools.nix
@@ -2,6 +2,21 @@
 
 {
   programs = {
+    # https://search.nixos.org/options?channel=unstable&query=programs.less
+    less = {
+      enable = true;
+
+      envVariables = {
+        # -X --no-init:             Avoids clearing the screen
+        # -q --quiet
+        # -R --RAW-CONTROL-CHARS:   Keeps colours
+        # -F --quit-if-one-screen:  Simply quits printing output if it fits
+        #    --mouse
+        "LESS" = "-XqRF --mouse";
+      };
+    };
+
+    # https://search.nixos.org/options?channel=unstable&query=programs.skim
     skim = {
       enable = true;
     };
@@ -13,16 +28,10 @@
 
       fd
       ripgrep
-      ## Logs
-      less
 
       # Hashing
       b3sum
       rblake3sum
     ];
-
-    variables = {
-      "PAGER" = "less -R";
-    };
   };
 }
