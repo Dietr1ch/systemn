ddfa0e4e6352c2d755bf62e03f53478b97119369
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-20 17:37:50 -0300

    Server,Monero: Open monero mesh port.
    This keeps the default of RPC-address being 127.0.0.1 and it's 18081 port closed

--- server/monero.nix
+++ server/monero.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   services = {
@@ -6,12 +6,30 @@
     monero = {
       enable = true;
 
+      # https://docs.getmonero.org/interacting/monerod-reference/
+      # https://docs.getmonero.org/interacting/monero-config-file/
+      # /var/lib/monero/monerod.conf
       limits = {
-        threads = 4;
+        threads = lib.mkDefault 4;
 
-        upload = 25; # 25kb/s
-        download = 25; # 25kb/s
+        upload = lib.mkDefault 100; # 100kb/s
+        download = lib.mkDefault 200; # 200kb/s
       };
+
+      extraConfig = ''
+        # https://docs.getmonero.org/interacting/monerod-reference/
+        # Optional pruning
+        prune-blockchain=1    # Pruning saves 2/3 of disk space w/o degrading functionality but contributes less to the network.
+        sync-pruned-blocks=1  # Allow downloading pruned blocks instead of prunning them yourself.
+      '';
     };
   };
+
+  # https://docs.getmonero.org/interacting/monerod-reference/#p2p-network
+  networking.firewall.allowedTCPPorts = [
+    # - 18080 TCP for incoming connections (mainnet default)
+    # - 18081 TCP for incoming RPC connections (can be configured to a different port with --rpc-bind-port)
+    # - 53 UDP for DNS (optional)
+    18080
+  ];
 }
