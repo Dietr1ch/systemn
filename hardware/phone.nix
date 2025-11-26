c659c92bcfaefb393953d70a1f468a5f8959732a
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-26 18:50:44 -0300

    Desktop,Phone: Enable KDEConnect

--- hardware/phone.nix
+++ hardware/phone.nix
@@ -6,5 +6,10 @@
     adb = {
       enable = true;
     }; # ..programs.adb
+
+    # https://search.nixos.org/options?channel=unstable&query=programs.kdeconnect
+    kdeconnect = {
+      enable = true;
+    }; # ..programs.kdeconnect
   }; # ..programs
 }
