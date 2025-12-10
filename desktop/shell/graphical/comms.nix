a150e0e4b0005322c49a111647ca7b8c43f6c043
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-10 13:52:31 -0300

    Desktop,Comms: Add thunderbird

--- desktop/shell/graphical/comms.nix
+++ desktop/shell/graphical/comms.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
 {
+  # https://search.nixos.org/options?channel=unstable&query=programs.thunderbird
+  programs = {
+    thunderbird = {
+      enable = true;
+    };
+  };
+
   environment = {
     systemPackages = with pkgs; [
       telegram-desktop
