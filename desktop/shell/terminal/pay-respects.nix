fb4ada16dc3dbf4dfef38007062aad596b6199fb
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-11 14:28:08 -0300

    Shell: Fix auto-detected package manager

--- desktop/shell/terminal/pay-respects.nix
+++ desktop/shell/terminal/pay-respects.nix
@@ -7,4 +7,11 @@
       enable = true;
     };
   };
+
+  environment = {
+    variables = {
+      # NOTE: Ensure this nix is the default package manager. (https://codeberg.org/iff/pay-respects/issues/34)
+      "_PR_PACKAGE_MANAGER" = "nix";
+    };
+  };
 }
