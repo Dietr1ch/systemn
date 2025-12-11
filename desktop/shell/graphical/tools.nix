44d7c5ba555ebc424075d9a3cf2b7959d895ae36
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-11 12:38:14 -0300

    Tools: Add kasts

--- desktop/shell/graphical/tools.nix
+++ desktop/shell/graphical/tools.nix
@@ -3,6 +3,8 @@
 {
   environment = {
     systemPackages = with pkgs; [
+      kdePackages.kasts # TODO: Find a new home for kasts
+
       transmission_4-qt6 # For pairing with ://server/transmission.nix
 
       gpu-viewer
