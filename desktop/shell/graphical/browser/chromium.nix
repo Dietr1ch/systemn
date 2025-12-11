1c0205e36de830b10592f3df12017772ba02fb5f
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-11 17:50:03 -0300

    Desktop,Chromium: Add default extensions

--- desktop/shell/graphical/browser/chromium.nix
+++ desktop/shell/graphical/browser/chromium.nix
@@ -8,6 +8,17 @@
 
       enablePlasmaBrowserIntegration = true;
       defaultSearchProviderSearchURL = lib.mkDefault "https://duckduckgo.com/?q={searchTerms}";
+
+      extensions = [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock origin. https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm
+        "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock. https://chromewebstore.google.com/detail/sponsorblock-for-youtube/mnjggcdmjocbbbhaepdhchncahnbgone
+        "oboonakemofpalcgghocfoadofidjkkk" # KeePassXC. https://chromewebstore.google.com/detail/keepassxc-browser/oboonakemofpalcgghocfoadofidjkkk
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader. https://chromewebstore.google.com/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh
+        "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus. https://chromewebstore.google.com/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne
+        "ldpochfccmkkmhdbclfhpagapcfdljkj" # Decentraleyes. https://chromewebstore.google.com/detail/decentraleyes/ldpochfccmkkmhdbclfhpagapcfdljkj
+        "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger. https://chromewebstore.google.com/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp
+        "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium. https://chromewebstore.google.com/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb
+      ]; # ..programs.chromium.extensions
     }; # ..programs.chromium
   }; # ..programs
 
