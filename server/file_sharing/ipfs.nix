cff9a3e53d84961b49c05881a10e49aec153ebe5
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-11 22:03:48 -0300

    Services,IPFS: Configure local IPFS node to allow requests from whitelisted IRIs

--- server/file_sharing/ipfs.nix
+++ server/file_sharing/ipfs.nix
@@ -23,6 +23,20 @@
             "/ip4/127.0.0.1/tcp/5001"
           ];
         };
+        API = {
+          # https://github.com/ipfs/kubo/blob/master/docs/config.md#apihttpheaders
+          HTTPHeaders = {
+            "Access-Control-Allow-Origin" = [
+              "http://localhost:3000"
+              "http://127.0.0.1:5001"
+              "https://webui.ipfs.io"
+            ];
+            "Access-Control-Allow-Methods" = [
+              "PUT"
+              "POST"
+            ];
+          };
+        };
       };
     };
   };
