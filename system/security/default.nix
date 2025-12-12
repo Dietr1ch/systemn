da4217c1c9cb0fe4bcef509a6ad91c8699e6b05a
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-11 21:15:49 -0300

    Misc: Cleanup commented config

--- system/security/default.nix
+++ system/security/default.nix
@@ -10,16 +10,6 @@
 
   # https://search.nixos.org/options?channel=unstable&query=security
   security = {
-    # https://nixos.wiki/wiki/Polkit
-    # https://search.nixos.org/options?channel=unstable&query=security.polkit
-    # polkit = {
-    #   enable = true;
-    # };
-
-    # https://search.nixos.org/options?channel=unstable&query=security.auditd
-    # auditd = {
-    #   enable = true;
-    # };
     # https://search.nixos.org/options?channel=unstable&query=security.sudo-rs
     sudo-rs = {
       enable = true;
