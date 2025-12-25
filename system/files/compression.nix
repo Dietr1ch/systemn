8b30e9ae014b20f2776d2a94f2ed0807c64652ab
Dietrich Daroch <Dietrich@Daroch.me>
2025-12-25 01:25:43 -0300

    Files,Compression: Add compression libraries

--- system/files/compression.nix
+++ system/files/compression.nix
@@ -3,11 +3,15 @@
 {
   environment = {
     systemPackages = with pkgs; [
+      # In sync with ~/Projects/nixland/core/compression.nix
       arj
+      brotli
+      lbzip2
       lrzip
       pbzip2
       pigz
       pixz
+      unzip
       xz
       zip
     ];
