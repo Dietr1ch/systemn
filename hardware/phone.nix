3f93b5b12d1bc5d07d661570bb406c206261c2e2
Dietrich Daroch <Dietrich@Daroch.me>
2026-01-06 15:16:25 -0300

    Phone: Update android config

--- hardware/phone.nix
+++ hardware/phone.nix
@@ -1,15 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs = {
-    # https://search.nixos.org/options?channel=unstable&query=programs.adb
-    adb = {
-      enable = true;
-    }; # ..programs.adb
-
     # https://search.nixos.org/options?channel=unstable&query=programs.kdeconnect
     kdeconnect = {
       enable = true;
     }; # ..programs.kdeconnect
   }; # ..programs
+
+  environment = {
+    systemPackages = with pkgs; [
+      android-tools
+    ];
+  }; # ..environment
 }
