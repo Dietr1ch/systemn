78bd160ed1461d126ac9a825feecd5a81ed71972
Dietrich Daroch <Dietrich@Daroch.me>
2026-01-09 01:49:13 -0300

    HW,GPU: Set amdgpu perf env variables

--- hardware/graphics/amd.nix
+++ hardware/graphics/amd.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }:
 
+# https://nixos.wiki/wiki/AMD_GPU
+# https://docs.mesa3d.org/drivers/radv.html
+# https://www.protondb.com/help/improving-performance
+# https://ritze03.github.io/cs2-linux-optimization/guide.html
 {
   environment = {
     systemPackages = with pkgs; [
       amdgpu_top
     ];
+
+    variables = {
+      "AMD_USERQ" = "1"; # https://docs.kernel.org/next/gpu/amdgpu/userq.html
+      "RADV_PERFTEST" = "aco"; # NOTE: Default since Mesa 20.2 (https://github.com/doitsujin/dxvk/issues/1905)
+    };
   };
 }
