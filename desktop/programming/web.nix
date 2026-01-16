dac90ecad8c53abe83154af6c6dac378505a0ad4
Dietrich Daroch <Dietrich@Daroch.me>
2026-01-16 11:54:25 -0300

    Programming,Web: Add htmx-lsp2

--- desktop/programming/web.nix
+++ desktop/programming/web.nix
@@ -5,6 +5,7 @@
     # Packages in sync with ~/Projects/nixland/programming/web.nix
     systemPackages = with pkgs; [
       emmet-ls
+      htmx-lsp2
 
       vscode-langservers-extracted
       prettier
