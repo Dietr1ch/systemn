795801b312dd8ebde1622c3e1586524a6a292b28
Dietrich Daroch <Dietrich@Daroch.me>
2026-03-02 09:10:25 -0300

    Workstation,Power,Network: Enable magic packet wol by default

--- hardware/laptop/power.nix
+++ hardware/laptop/power.nix
@@ -10,6 +10,18 @@
     cpuFreqGovernor = lib.mkDefault "powersave";
   };
 
+  networking = {
+    # https://search.nixos.org/options?channel=unstable&query=networking.networkmanager
+    networkmanager = {
+      # /etc/NetworkManager/NetworkManager.conf::connection
+      # https://networkmanager.dev/docs/api/latest/NetworkManager.conf.html
+      connectionConfig = {
+        "ethernet.wake-on-lan" = lib.mkDefault "ignore";
+        "wifi.wake-on-lan" = lib.mkDefault "ignore";
+      };
+    };
+  };
+
   services = {
     # https://search.nixos.org/options?channel=unstable&query=services.thermald
     thermald = {
--- hardware/workstation/power.nix
+++ hardware/workstation/power.nix
@@ -9,4 +9,16 @@
   powerManagement = {
     cpuFreqGovernor = lib.mkDefault "ondemand";
   };
+
+  networking = {
+    # https://search.nixos.org/options?channel=unstable&query=networking.networkmanager
+    networkmanager = {
+      # /etc/NetworkManager/NetworkManager.conf::connection
+      # https://networkmanager.dev/docs/api/latest/NetworkManager.conf.html
+      connectionConfig = {
+        "ethernet.wake-on-lan" = lib.mkDefault "magic";
+        "wifi.wake-on-lan" = lib.mkDefault "ignore";
+      };
+    };
+  };
 }
