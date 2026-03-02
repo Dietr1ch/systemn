6bd96d1019cf9081fec847fec8367bd91a47c555
Dietrich Daroch <Dietrich@Daroch.me>
2026-03-02 18:52:28 -0300

    Misc: Add link

--- server/web.nix
+++ server/web.nix
@@ -4,14 +4,17 @@
   # https://nixos.wiki/wiki/Nginx
   services = {
     # https://search.nixos.org/options?channel=unstable&query=services.nginx
+    # ~/Projects/nixpkgs/nixos/modules/services/web-servers/nginx/default.nix
     nginx = {
       enable = true;
 
-      recommendedGzipSettings = true;
       recommendedOptimisation = true;
+      # Connection
       recommendedProxySettings = true;
       # recommendedTlsSettings = true;
 
+      # Compression
+      recommendedGzipSettings = true;
       # Config file mentioned in /etc/systemd/system/nginx.service
       virtualHosts = {
 
