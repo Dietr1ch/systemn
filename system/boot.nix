1407757f993b09b8498aa638da5eecd58311c0d8
Dietrich Daroch <Dietrich@Daroch.me>
2026-04-21 11:19:37 -0400

    System,Boot: Specify a default 5s bootloader menu timeout.

--- system/boot.nix
+++ system/boot.nix
@@ -1,11 +1,13 @@
-{
-  ...
-}:
+{ lib, ... }:
 
 {
   boot = {
     kernelParams = [
       # "copytoram"  # I'm not sure it really benefits performance
     ];
+
+    loader = {
+      timeout = lib.mkDefault 5;
+    };
   };
 }
