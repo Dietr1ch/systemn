162bf520536e187c4cc3407e8dffb010372e5e7f
Dietrich Daroch <Dietrich@Daroch.me>
2026-06-12 09:18:04 -0400

    Nix: Upgrade config

--- system/base/default.nix
+++ system/base/default.nix
@@ -21,21 +21,20 @@
   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/system/dbus.nix
   services.dbus.implementation = "broker";
 
-  # systemd.user.settings
   systemd = {
-    # /etc/systemd/user.conf
-
     user = {
-      # https://search.nixos.org/options?channel=unstable&show=systemd.user.extraConfig
-      extraConfig = ''
-        # https://www.mankier.com/5/systemd-system.conf
+      # /etc/systemd/user.conf
+      settings = {
+        Manager = {
+          # https://www.mankier.com/5/systemd-system.conf
 
-        # https://www.mankier.com/5/systemd-system.conf#Timeouts_and_Rate_Limits
-        DefaultTimeoutStartSec=5s
-        DefaultTimeoutStopSec=20s
-        DefaultTimeoutAbortSec=20s
-        # DefaultRestartSec=100ms
-      '';
+          # https://www.mankier.com/5/systemd-system.conf#Timeouts_and_Rate_Limits
+          "DefaultTimeoutStartSec" = "5s";
+          "DefaultTimeoutStopSec" = "20s";
+          "DefaultTimeoutAbortSec" = "20s";
+          # "DefaultRestartSec"="100ms";
+        };
+      };
     };
   };
 }
