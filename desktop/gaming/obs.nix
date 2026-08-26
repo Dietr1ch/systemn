90870deb1057389fe2056ede2369aa28fcfe9201
Dietrich Daroch <Dietrich@Daroch.me>
2026-08-26 10:20:41 -0400

    Desktop,Gaming: Mark obs-source-switcher as broken

--- desktop/gaming/obs.nix
+++ desktop/gaming/obs.nix
@@ -15,7 +15,7 @@
         obs-backgroundremoval
         obs-gstreamer
         obs-pipewire-audio-capture
-        obs-source-switcher
+        # BROKEN: obs-source-switcher
         obs-vaapi
         obs-vkcapture
         wlrobs
